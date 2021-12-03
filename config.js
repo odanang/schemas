fb7f9b95dfe340ee301233864c5dcd41319276b8
robert <truongduchuy910@gmail.com>
2021-12-03 22:26:46 +0700

    ...

--- config.js
+++ config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  models: [
-    "InteractiveComment",
-    "InteractiveReaction",
-    "Interactive",
-    "PostTag",
-    "Post",
-    "Relationship",
-    "Translate",
-    "UploadFile",
-    "UploadImage",
-    "User",
-  ],
+  models: ["User"],
 };
